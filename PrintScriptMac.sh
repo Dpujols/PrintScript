4acd65cb2bce64a7654b7a1ea4caa54b96fbd462
Dpujols <33701241+Dpujols@users.noreply.github.com>
2021-04-03 14:54:49 -0400

    Added Preliminary Version for MAC
    This is a testing version of the script for mac, I have little experience with BASH and need some time to learn the syntax. This version does not install papercut but should install generic postscript printers.

---
+++ PrintScriptMac.sh
@@ -0,0 +1,6 @@
+echo off
+
+cat tail -n +2 Devices.txt | while read line; do
+	lpadmin -p $line -L  "$line" -E -v ipp://srv-pc-print.adm.suffolk.edu/$line -P /Library/Printers/PPDs/Contents/Resources/Generic.ppd	
+done
+
