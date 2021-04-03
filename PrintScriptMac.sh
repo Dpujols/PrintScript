f7430a8b5c976fd01135170d67187c86c674a24d
Dpujols <33701241+Dpujols@users.noreply.github.com>
2021-04-03 14:56:08 -0400

    Update PrintScriptMac.sh

--- PrintScriptMac.sh
+++ PrintScriptMac.sh
@@ -1,4 +1,3 @@
-echo off
 
 cat tail -n +2 Devices.txt | while read line; do
 	lpadmin -p $line -L  "$line" -E -v ipp://srv-pc-print.adm.suffolk.edu/$line -P /Library/Printers/PPDs/Contents/Resources/Generic.ppd	
