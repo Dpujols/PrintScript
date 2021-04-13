a9967a37fe368ea4fbc2d154a88a23c57384755f
Dpujols <33701241+Dpujols@users.noreply.github.com>
2021-04-13 13:02:19 -0400

    Format Change in how Devices.txt is handled
    - Devices.txt now has a Devices_Law.txt counterpart intended for printers on srv-pc-ls-print.adm.suffolk.edu
    - The windows version is being prepared for an update for optinal installs of certain domain printers
    - Devices_Law and Devices.txt now need the first three lines skipped

---
+++ PrintersLoop.bat
@@ -0,0 +1,28 @@
+::This program installs the standard set of printers for windows
+::Diego P. 2021
+@echo off
+:: Creates a dirty list of installed network printers
+reg query HKEY_CURRENT_USER\Printers\Connections >> "%userprofile%\Desktop\installedprinters.txt"
+
+:: Cleans up the list then deletes the old file
+(for /f "tokens=3 delims=, " %%X in (%userprofile%\Desktop\installedprinters.txt) DO (
+	echo %%X
+	echo/
+)) >"%userprofile%\Desktop\PrinterNames.txt"
+DEL %userprofile%\Desktop\installedprinters.txt
+
+@echo off
+
+:: Updates/Installs papercut
+%SystemRoot%\explorer.exe \\srv-pc-print.adm.suffolk.edu\PCClient\win\client-local-install.exe
+
+
+:: Checks device list and installs them then cleans up 
+for /f "delims= skip=3" %%G in (Devices.txt) DO (
+	find [/i] "%%G" [%userprofile%\Desktop\PrinterNames.txt]
+	if %errorlevel% EQU 1 ( %SystemRoot%\explorer.exe \\srv-pc-print.adm.suffolk.edu\%%G
+	)
+)
+DEL %userprofile%\Desktop\PrinterNames.txt
+
+& exit
